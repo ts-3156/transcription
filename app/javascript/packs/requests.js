81528e00f871547db3cab5b6ff2db65e5dce3a78
ts-3156 <ts_3156@yahoo.co.jp>
2020-04-10 18:30:00 +0900

    Many changes 3!

--- app/javascript/packs/requests.js
+++ app/javascript/packs/requests.js
@@ -1,3 +1,26 @@
+(function ($) {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
+
+  $(".popover-dismiss").popover({
+    trigger: "focus"
+  });
+
+  var navbarCollapse = function () {
+    if ($(".navbar.fixed-top").length === 0) {
+      return;
+    }
+    if ($(".navbar.fixed-top").offset().top > 0) {
+      $(".navbar").addClass("navbar-scrolled");
+    } else {
+      $(".navbar").removeClass("navbar-scrolled");
+    }
+  };
+  navbarCollapse();
+  $(window).scroll(navbarCollapse);
+
+})(jQuery);
+
 class Form {
   constructor() {
     this.$el = $('#model_form');
@@ -155,4 +178,12 @@ class Util {
 
 $(function () {
   var form = new Form();
+
+  // Smooth scrolling when clicking an anchor link
+  $('a[href^="#"]').click(function () {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+    return false;
+  });
 });
