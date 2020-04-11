ae281b9c35f92e226300246a7c5df64ed3b2ab2b
ts-3156 <ts_3156@yahoo.co.jp>
2020-04-11 10:18:59 +0900

    Many changes 4!

--- app/helpers/audios_helper.rb
+++ app/helpers/audios_helper.rb
@@ -5,6 +5,21 @@ module AudiosHelper
       t('datetime.distance_in_words.x_seconds.other', count: duration)
     elsif duration < 3600
       t('datetime.distance_in_words.x_minutes.other', count: duration / 60)
+    else
+      t('datetime.distance_in_words.about_x_hours.other', count: duration / 3600) + duration_text(duration % 3600)
     end
+  end
+
+  def remaining_duration_text(requests)
+    duration = t('layouts.application.free_duration_seconds') - requests.map(&:audio).compact.map(&:duration).sum
+    duration_text(duration)
+  end
+
+  def expiration_kinds
+    @expiration_kinds ||= {
+        guest: t('layouts.application.expiration_for.guest'),
+        login: t('layouts.application.expiration_for.login'),
+        paid: t('layouts.application.expiration_for.paid')
+    }
   end
 end
